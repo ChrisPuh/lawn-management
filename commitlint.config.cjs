0e8fa949f264a1a78e5b09e4356d7a6bef736e8a
Chris <59114509+ChrisPuh@users.noreply.github.com>
2024-12-03 20:37:31 +0100

    feat(config): update commit 2.0validation setup

---
+++ commitlint.config.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'feat',
+          'fix',
+          'docs',
+          'style',
+          'refactor',
+          'perf',
+          'test',
+          'chore',
+          'ci',
+          'revert'
+        ]
+      ],
+      'scope-enum': [
+        2,
+        'always',
+        [
+          'auth',
+          'tasks',
+          'lawn',
+          'garden',
+          'ui',
+          'db',
+          'config'
+        ]
+      ],
+      'scope-empty': [2, 'never'],
+      'subject-empty': [2, 'never'],
+      'subject-full-stop': [2, 'never', '.'],
+      'subject-case': [
+        2,
+        'never',
+        ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+      ],
+    }
+  };
