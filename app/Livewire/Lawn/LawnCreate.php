68ecd347d432fc6d7212c5940e89d27a53c28520
Chris <59114509+ChrisPuh@users.noreply.github.com>
2025-01-02 21:01:30 +0100

    feat(test): added LawnRulesTest

--- app/Livewire/Lawn/LawnCreate.php
+++ app/Livewire/Lawn/LawnCreate.php
@@ -62,7 +62,7 @@ final class LawnCreate extends Component implements HasForms
                     ->nullable()
                     ->label('Grassorte')
                     ->options(collect(GrassSeed::cases())->mapWithKeys(
-                        fn(GrassSeed $type) => [$type->value() => $type->label()]
+                        fn (GrassSeed $type) => [$type->value() => $type->label()]
                     ))
                     ->rules(LawnRules::grassSeedRules())
                     ->validationMessages(LawnRules::messages()),
@@ -71,7 +71,7 @@ final class LawnCreate extends Component implements HasForms
                     ->nullable()
                     ->label('Rasentyp')
                     ->options(collect(GrassType::cases())->mapWithKeys(
-                        fn(GrassType $type) => [$type->value() => $type->label()]
+                        fn (GrassType $type) => [$type->value() => $type->label()]
                     ))
                     ->rules(LawnRules::typeRules())
                     ->validationMessages(LawnRules::messages()),
