bff506c836f758a8ec670afea1092ba89731f00e
Chris <59114509+ChrisPuh@users.noreply.github.com>
2025-01-12 18:41:50 +0100

    chore(pint): pint

--- app/Enums/LawnImageType.php
+++ app/Enums/LawnImageType.php
@@ -8,14 +8,14 @@ enum LawnImageType: string
 {
     case BEFORE = 'before';
     case AFTER = 'after';
-    case GENERAL= 'general';
+    case GENERAL = 'general';
 
     public function label(): string
     {
         return match ($this) {
             self::BEFORE => 'Vorher',
             self::AFTER => 'Nachher',
-            self::GENERAL=>'General'
+            self::GENERAL => 'General'
         };
     }
 }
